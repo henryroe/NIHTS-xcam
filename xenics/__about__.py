d6fb436e6786546f59caf5d40beb032c5750f3ca
Henry Roe <hroe@hroe.me>
2017-04-17 17:05:17 -0700

    updated

--- xenics/__about__.py
+++ xenics/__about__.py
@@ -5,12 +5,12 @@ __all__ = [
     "__email__", "__license__", "__copyright__",
 ]
 
-__title__ = "xenics_pluto"
-__summary__ = "This package contains a stripped down version of Henry Roe's Xenics XEVA near-infrared software for Pluto occultation observations."
-__uri__ = "https://github.com/henryroe/xenics_pluto"
+__title__ = "NIHTS-xcam"
+__summary__ = "Basic control software for the Xenics XEVA near-infrared slit-viewing camera on Lowell DCT NIHTS."
+__uri__ = "https://github.com/henryroe/NIHTS-xcam"
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-__version__ = "0.1.0dev"
+__version__ = "0.2.0dev"
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in __version__
@@ -22,4 +22,4 @@ __author__ = "Henry Roe"
 __email__ = "hroe@hroe.me"
 
 __license__ = "MIT License"
-__copyright__ = "2015 %s" % __author__
+__copyright__ = "2017 %s" % __author__
